5bbea91aece4dfafb4ad949c551e5616f030a03d
liuwangchun <liuwangchun@xiniaoyun.com>
2019-02-18 14:13:15 +0800

    Jwt

---
+++ src/main/java/com/mystudy/multilearning/jwt/consts/JwtConstans.java
@@ -0,0 +1,13 @@
+package com.mystudy.multilearning.jwt.consts;
+
+/**
+ * @ClassName JwtConstans
+ * @Description TODO
+ * @Author MrLiu
+ */
+public class JwtConstans {
+
+    public static final String JWT_KEY_ID = "id";
+    public static final String JWT_KEY_USER_NAME = "username";
+
+}
